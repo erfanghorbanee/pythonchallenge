cfb192e8638fe9749f26ddc1682b18a2636ec17c
efi <erfanghorbanee@gmail.com>
2022-12-20 14:37:57 +0330

    Style improved by black

--- lvl0.py
+++ lvl0.py
@@ -1,3 +1,3 @@
 # lvl0
 
-print(pow(2, 38)) # put the result(274877906944) in url instead of 0.
+print(pow(2, 38))  # put the result(274877906944) in url instead of 0.
--- lvl5.py
+++ lvl5.py
@@ -1,25 +1,28 @@
 import requests
 
-params = { "nothing":"12345" }
+params = {"nothing": "12345"}
 while True:
     flag = 0
-    r = requests.get("http://www.pythonchallenge.com/pc/def/linkedlist.php", params= params )
-    n = len(r.text.split()) -  1
+    r = requests.get(
+        "http://www.pythonchallenge.com/pc/def/linkedlist.php", params=params
+    )
+    n = len(r.text.split()) - 1
     print(r.text)
     # print(r.text.split()[n])
-    
+
     if flag == 1:
-        params["nothing"] = int(params["nothing"])/2
+        params["nothing"] = int(params["nothing"]) / 2
         flag = 0
     else:
         params["nothing"] = r.text.split()[n]
         flag = 0
-            
-    if not params["nothing"].isdigit():  # when we get this sentence: Yes. Divide by two and keep going.
+
+    if not params[
+        "nothing"
+    ].isdigit():  # when we get this sentence: Yes. Divide by two and keep going.
         flag = 1
-        
-    if r.text == "peak.html": #final answer
+
+    if r.text == "peak.html":  # final answer
         break
 
 # now visit http://www.pythonchallenge.com/pc/def/peak.html :)
-
